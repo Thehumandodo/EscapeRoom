7ae6b6f02851c186abff4f463317d16458484485
Ricardo <yuwonoricardo@gmail.com>
2021-10-13 21:44:11 +0700

    added a new scene for keypad

---
+++ MakerlabEscapeRoom/Assets/Scripts/BackButton.cs
@@ -0,0 +1,30 @@
+using System;
+using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+using UnityEngine.SceneManagement;
+
+public class BackButton : MonoBehaviour
+{
+    public Button backButton;
+    
+    
+    // Start is called before the first frame update
+    void Start()
+    {
+        Button btn = backButton.GetComponent<Button>();
+        btn.onClick.AddListener(clicked);
+    }
+
+    void clicked(){
+        SceneManager.LoadScene("SampleScene");
+    }
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+    
+}
